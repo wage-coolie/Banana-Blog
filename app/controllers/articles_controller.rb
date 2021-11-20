5dc35d25774f5edd3a92d50ab354d1bfae6922ef
utkarsh <endgithub@protnmail.com>
2021-11-20 18:23:56 +0530

    CRUD

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,12 +1,31 @@
 class ArticlesController < ApplicationController
+	before_action :set_article, only: [:edit, :update, :destroy, :show]
 
-
+	def index
+		@articles = Article.all
+	end
 
 	def new
 		@article = Article.new
 	end
+
+	def edit
+		
+	end
+
+	def update
+		
+		if @article.update(article_params)
+			flash[:notice] = "Successfully updates..."
+			redirect_to article_path(@article)
+		else
+			render "edit"
+		end
+
+	end
+
 	def create
-		@article = Article.new(article_param)
+		@article = Article.new(article_params)
 		if @article.save
 			flash[:notice] = "Successfully created..."
 			redirect_to article_path(@article)
@@ -17,11 +36,23 @@ class ArticlesController < ApplicationController
 	end
 
 	def show
-		@article=Article.find(params[:id])
+		
+	end
+
+	def destroy
+		
+		@article.destroy
+		flash[:notice] = "Successfully destroyed..."
+		redirect_to articles_path
 	end
 
 	private
-	def article_param
+
+	def set_article
+		@article = Article.find(params[:id])
+
+	end
+	def article_params
 		params.require(:article).permit(:title,:description)
 	end
 
